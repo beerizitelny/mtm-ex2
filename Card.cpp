ffd0cf35950a8756332b56e98837f9b45f780aae
beerizitelny <beerizitelny@campus.technion.ac.il>
2023-05-13 13:59:22 +0300

    card is ready

---
+++ Card.cpp
@@ -0,0 +1,54 @@
+//
+// Created by baeri on 13/05/2023.
+//
+#include "Card.h"
+
+Card::Card(CardType type, const CardStats& stats){
+    this->m_effect = type;
+    this->m_stats = stats;
+}
+
+void Card::applyEncounter(Player& player) const{
+    switch (this->m_effect) {
+        case CardType::Battle:
+            printBattleResult(player.getAttackStrength() >= this->m_stats.force);
+            if (player.getAttackStrength() >= this->m_stats.force){
+                player.levelUp();
+                player.addCoins(this->m_stats.loot);
+            }
+            else{
+                player.damage(this->m_stats.hpLossOnDefeat);
+            }
+            break;
+        case CardType::Heal:
+            if (player.pay(this->m_stats.cost)){
+                player.heal(this->m_stats.heal);
+            }
+            break;
+        case CardType::Buff:
+            if (player.pay(this->m_stats.cost)){
+                player.buff(this->m_stats.buff);
+            }
+        case CardType::Treasure:
+            player.addCoins(this->m_stats.loot);
+
+    }
+}
+
+void Card::printInfo() const{
+    switch (this->m_effect) {
+        case CardType::Battle:
+            printBattleCardInfo(this->m_stats);
+            break;
+        case CardType::Heal:
+            printHealCardInfo(this->m_stats);
+            break;
+        case CardType::Buff:
+            printBuffCardInfo(this->m_stats);
+            break;
+        case CardType::Treasure:
+            printTreasureCardInfo(this->m_stats);
+            break;
+    }
+}
+
